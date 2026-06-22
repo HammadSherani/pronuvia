344baa16414c2a9cecbb1583e3c40e83a55ab341
HammadSherani <hammadk426623@gmail.com>
2026-06-23 02:37:40 +0500

    home page changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import { redirect } from "next/navigation";
 
 export default function RootPage() {
-  redirect("/login");
+  // redirect("/login");
+
+  return (
+    <h1>Welcome to the Root Page</h1>
+  )
 }
